d2fb6f0ed61e4de7fae674525d33117eb017ea95
vishnuchandrappan <cvishnu70@gmail.com>
2020-06-07 20:54:36 +0530

    added yt link

--- src/Components/Pages/PegSystem.js
+++ src/Components/Pages/PegSystem.js
@@ -1,10 +1,19 @@
+import PlayArrowRoundedIcon from "@material-ui/icons/PlayArrowRounded";
 import React from "react";
-
 export const PegSystem = () => {
   return (
-    <div className="container">
-      <h1>The Peg System</h1>
-      <main>coming soon...</main>
+    <div className="container peg-system">
+      <div className="img-container">
+        <img
+          src="https://i.ytimg.com/vi/PK-wOZghJjg/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAyD8BNgIRg-jezW59r5jCcAM71eQ"
+          alt="youTube_thumbnail"
+        />
+      </div>
+      <div className="btn-container">
+        <a href="https://www.youtube.com/watch?v=PK-wOZghJjg" className="button">
+          Watch Video <PlayArrowRoundedIcon/>
+        </a>
+      </div>
     </div>
   );
 };
